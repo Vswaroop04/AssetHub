e498ea0983a1c7ef41061e8ba89f37804cdbbeb3
vishnuoruphones <vswaroop04@gmail.com>
2024-01-31 21:18:08 +0530

    chore : trpc setup completed

--- src/Server.ts
+++ src/Server.ts
@@ -1,11 +1,30 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './trpc'
+import { IncomingMessage } from 'http'
+
+import { inferAsyncReturnType } from '@trpc/server'
 
 const app = express();
 
 const PORT = Number(process.env.PORT) || 3000;
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+})
 
+export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
+
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
 const start = async () => {
   const payload = await getPayloadClient({
     initOptions: {
@@ -15,6 +34,14 @@ const start = async () => {
       },
     },
   });
+
+    app.use(
+    '/api/trpc',
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  )
 
   app.use((req, res) => {
     nextHandler(req, res);
